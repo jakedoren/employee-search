1200113f57e621de601f3f4d20724cc1fb0639ce
Jacob Doren <Jacob Doren>
2021-03-30 00:13:12 -0400

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import employeedata from '../employee.json'
+import './main.css'
+
+const Main = () => {
+    const [searchInput, setSearchInput] = useState("")
+    
+
+
+    
+
+    return (
+        <div className="main">
+            <input onChange={(e) => setSearchInput(e.target.value)} placeholder="Search..."/>
+            {employeedata.filter((val) => {
+                if(searchInput === "") {
+                    return (
+                        val
+                    )
+                } else if(val.first_name.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase())) {
+                    return val
+                }
+            }).map((val,key) => {
+                return(
+                <div className="employee" key={key} >
+                    <p>{val.first_name}</p> <p>{val.last_name}</p> <p>{val.email}</p> <p>{val.employee_id}</p>
+                </div> 
+                )
+            })}
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="nav">
+            <div className="nav-container">
+                <h1>MaxCash</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
