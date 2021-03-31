3dce764691fef82194bb25375f933f47b23b6ad8
Jacob Doren <Jacob Doren>
2021-03-30 23:22:05 -0400

    style changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import './main.css'
 
 const Main = () => {
     const [searchInput, setSearchInput] = useState("")
+    const [filteredArray, setFilteredArray] = useState(employeedata)
     
 
 
@@ -11,8 +12,18 @@ const Main = () => {
 
     return (
         <div className="main">
+            <div>{filteredArray.length}</div>
             <input onChange={(e) => setSearchInput(e.target.value)} placeholder="Search..."/>
-            {employeedata.filter((val) => {
+            <div className="employee-head-wrapper">
+                <div className="employee-head">
+                <p>First Name</p>
+                <p>Last Name</p>
+                <p>Email</p>
+                <p>Employee ID</p>
+            </div>
+            </div>
+            
+            { setFilteredArray(employeedata.filter((val) => {
                 if(searchInput === "") {
                     return (
                         val
@@ -20,13 +31,17 @@ const Main = () => {
                 } else if(val.first_name.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase())) {
                     return val
                 }
-            }).map((val,key) => {
+            }))
+            .map((val,key) => {
                 return(
-                <div key={key} className="employee">
-                    <p>{val.first_name}</p> <p>{val.last_name} </p> <p>{val.email}</p> <p>{val.employee_id}</p>
-                </div>
+                    <div className="employee-wrapper">
+                        <div key={key} className="employee">
+                            <p>{val.first_name}</p> <p>{val.last_name} </p> <p>{val.email}</p> <p>{val.employee_id}</p>
+                        </div>
+                    </div>
                 )
-            })}
+            })
+            }
         </div>
     )
 }
